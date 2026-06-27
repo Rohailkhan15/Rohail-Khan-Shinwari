be8458bc3b4ee76e4448b4dc907acf6770074a1f
Rohailkhan15 <thomaspatrick54340@gmail.com>
2026-06-27 22:56:59 +0500

    step 11 Set up Sanity project + schema

---
+++ sanity/schemas/blogPost.ts
@@ -0,0 +1,92 @@
+export default {
+  name: 'blogPost',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+      description: '1-2 sentence summary for the card',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Essay', value: 'Essay' },
+          { title: 'Build Log', value: 'Build Log' },
+          { title: 'Research', value: 'Research' },
+          { title: 'Micro-thought', value: 'Micro-thought' },
+        ],
+      },
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+        },
+        {
+          type: 'code',
+        },
+      ],
+    },
+    {
+      name: 'readingTime',
+      title: 'Reading Time (minutes)',
+      type: 'number',
+      description: 'Calculated from word count, but can be manually set',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'coverImage',
+    },
+  },
+}
