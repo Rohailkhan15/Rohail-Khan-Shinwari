20cf35b78bcd73979e3a4bdd83ae0a9f2f56c555
Shinwari <thomaspatrick54340@gmail.com>
2026-08-20 20:33:14 +0500

    adding sanity appid

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
+  deployment: {
+    appId: 'llp4vpwo59wv68j7lxgmq2iq',
+  },
 })
