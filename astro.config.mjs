4332bb6ded92e235885a50580d6ba37ac932c8a0
Shinwari <thomaspatrick54340@gmail.com>
2026-08-31 03:10:27 +0500

    Fixing speed and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourdomain.com',
+  site: 'https://rohail-khan-shinwari.pages.dev',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   },
